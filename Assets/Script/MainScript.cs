7fed2c3ad810d1702007f822613701324603972d
123123hhh-gif <2651704068@qq.com>
2026-02-12 08:01:40 +0800

    implement basic game loop and state management

--- Assets/Script/MainScript.cs
+++ Assets/Script/MainScript.cs
@@ -1,19 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainScript : MonoBehaviour
 {
+    public AudioClip bgmWarriors; // Background music clip for main menu 
 
-     public AudioClip bgmWarriors;
-    // Start is called before the first frame update
     void Start()
     {
-        AudioManager.Instance.PlayBGM(bgmWarriors);
+        AudioManager.Instance.PlayBGM(bgmWarriors); // Play menu BGM
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -21,31 +17,31 @@ public class MainScript : MonoBehaviour
 
     public void onBtnLevel1()
     {
-         SceneManager.LoadScene("L1");
+        SceneManager.LoadScene("L1"); // Placeholder scene loading; full level structure and progression system not finalized yet
     }
 
     public void onBtnLevel2()
     {
-         SceneManager.LoadScene("L2");
+        SceneManager.LoadScene("L2"); // Temporary direct scene jump before implementing proper level selection logic
     }
 
     public void onBtnLevel3()
     {
-         SceneManager.LoadScene("L3");
+        SceneManager.LoadScene("L3"); // Scene entry defined early; gameplay content may still be under development
     }
 
     public void onBtnLevel4()
     {
-         SceneManager.LoadScene("L4");
+        SceneManager.LoadScene("L4"); // Reserved level slot; structure defined before full content implementation
     }
 
     public void onBtnLevel5()
     {
-         SceneManager.LoadScene("L5");
+        SceneManager.LoadScene("L5"); // Scene reference prepared in advance for future expansion
     }
 
-     public void onBtnLevel6()
+    public void onBtnLevel6()
     {
-         SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("SampleScene"); 
     }
 }
